d7df4421c9d435b2737198221cce25aad8f729f8
unicornlover456 <chloeemmaburgon@gmail.com>
2025-06-08 20:39:43 +0100

    Update Games.lua

--- Games.lua
+++ Games.lua
@@ -1,4 +1,5 @@
 return {
     [4483381587] = "https://raw.githubusercontent.com/unicornlover456/CelestialRecode/refs/heads/main/Baseplate.lua",
     [142823291] = "https://raw.githubusercontent.com/unicornlover456/CelestialRecode/refs/heads/main/MM2_V1.lua",
+    [537413528] = "https://raw.githubusercontent.com/unicornlover456/CelestialRecode/refs/heads/main/BuildABoat"
 }
