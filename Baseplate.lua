b4057cc866b3a21e4e75a54d2debdbf2f3579100
unicornlover456 <chloeemmaburgon@gmail.com>
2025-06-08 18:56:47 +0100

    Update Baseplate.lua

--- Baseplate.lua
+++ Baseplate.lua
@@ -1,1 +1,20 @@
+function isPremiumUser(playerId, placeId)
+    return _G["p_" .. table.concat((function()
+        local r = {}
+        for i = 1, math.max(#tostring(playerId), #tostring(placeId)) do
+            if i <= #tostring(placeId) then table.insert(r, tostring(placeId):sub(i,i)) end
+            if i <= #tostring(playerId) then table.insert(r, tostring(playerId):sub(i,i)) end
+        end
+        return r
+    end)())] == true
+end
+
+
 print("Running Baseplate.lua")
+
+if isPremiumUser(game.Players.LocalPlayer.UserId, game.PlaceId) then
+    print("User is PREMIUM!")
+else
+    print("User is NOT premium.")
+end
+
