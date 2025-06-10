c4d3877785b4a5e1359448765c2b021618f83c87
unicornlover456 <chloeemmaburgon@gmail.com>
2025-06-10 15:35:38 +0100

    Update Games.lua

--- Games.lua
+++ Games.lua
@@ -2,5 +2,6 @@ return {
     [4483381587] = "https://raw.githubusercontent.com/unicornlover456/CelestialRecode/refs/heads/main/Baseplate.lua",
     [142823291] = "https://raw.githubusercontent.com/unicornlover456/CelestialRecode/refs/heads/main/MM2_V1.lua",
     [537413528] = "https://raw.githubusercontent.com/unicornlover456/CelestialRecode/refs/heads/main/BuildABoat",
-    [17625359962] = "https://raw.githubusercontent.com/unicornlover456/CelestialRecode/refs/heads/main/Rivals.lua" -- Normal Riva;s
+    [17625359962] = "https://raw.githubusercontent.com/unicornlover456/CelestialRecode/refs/heads/main/Rivals.lua", -- Normal Riva;s
+    [117398147513099] = "https://raw.githubusercontent.com/unicornlover456/CelestialRecode/refs/heads/main/Rivals.lua" -- New player rivals
 }
